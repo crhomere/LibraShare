f46bea900ad9b0fa2344f1e0bb8b1d134f91efbe
Monisha Jacob <monishacherian@gmail.com>
2023-05-17 20:05:15 -0400

    Spring Initializercontents

---
+++ libra-share-backend/src/main/java/com/librashare/app/LibrashareApplication.java
@@ -0,0 +1,13 @@
+package com.librashare.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibrashareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibrashareApplication.class, args);
+	}
+
+}
