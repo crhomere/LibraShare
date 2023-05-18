b25a473d4520aef29a46a6399e610a5d6a4b5d25
Monisha Jacob <monishacherian@gmail.com>
2023-05-17 20:05:15 -0400

    Spring Initializercontents

---
+++ libra-share-backend/src/test/java/com/librashare/app/LibrashareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librashare.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrashareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
