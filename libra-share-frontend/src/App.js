a48ea20b8e6282789119318687361ab2f45b93c7
OxanaSF <oxanatango@gmail.com>
2023-05-25 17:29:52 -0700

    add registration, navigation to user profile

--- libra-share-frontend/src/App.js
+++ libra-share-frontend/src/App.js
@@ -1,9 +1,12 @@
 import  { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import HomePage from './components/HomePage/HomePage';
-import LoginForm from './components/LoginForm/LoginForm';
-import SignupForm from './components/SignupForm/SignupForm';
+
+import EntryPage from './pages/LandingPage/LandingPage';
+import HomePage from './pages/HomePage/HomePage';
+import Register from './pages/Register/Register';
+import Navbar from './components/Navbar/Navbar';
+import Cart from './components/Cart/Cart';
 import BookList from './components/BookList/BookList';
 import BookDetails from './components/BookDetails/BookDetails';
 import ReviewForm from './components/ReviewForm/ReviewForm';
@@ -16,14 +19,18 @@ const App = () => {
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/signup" element={<SignupForm />} />
+          <Route path="/" element={<EntryPage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/home" element={<HomePage />} />
+
+          <Route path="*" element={<ErrorPage />} />
+
+          <Route path="/cart" element={<Cart />} />
           <Route path="/library" element={<BookList />} />
           <Route path="/books/:bookId" element={<BookDetails />} />
           <Route path="/books/:bookId/review" element={<ReviewForm />} />
           <Route path="/user/books" element={<UserBooks />} />
-          <Route path="/user/profile" element={<UserProfile />} />
+          <Route path="/user-profile/:userId" element={<UserProfile />} />
           <Route path="/search" element={<SearchBar />} />
         </Routes>
       </Router>
