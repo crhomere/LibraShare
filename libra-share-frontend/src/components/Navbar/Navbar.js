3dcfd56954649a17b60efed94ed1c982d38757fd
OxanaSF <oxanatango@gmail.com>
2023-05-26 18:00:13 -0700

    create SideNavbar for main page/dash

--- libra-share-frontend/src/components/Navbar/Navbar.js
+++ libra-share-frontend/src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBook } from '@fortawesome/free-solid-svg-icons';
 
