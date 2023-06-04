0f361ea9bf17e423b01899ff2a4d87c8ec13fa03
christopher.homere <christopher.homere@gdit.com>
2023-06-04 17:33:29 -0400

    Update to Google Map Component

--- libra-share-frontend/src/components/GoogleMap/Map.js
+++ libra-share-frontend/src/components/GoogleMap/Map.js
@@ -1,31 +1,69 @@
 import React, { useState, useCallback, useEffect } from 'react';
-import { GoogleMapsProvider } from '@ubilabs/google-maps-react-hooks';
+import { GoogleMapsProvider, useDistanceMatrixService } from '@ubilabs/google-maps-react-hooks';
 import axios from 'axios';
+
+const useSortedAddresses = (addresses, coordinates) => {
+  const [sortedAddresses, setSortedAddresses] = useState([]);
+
+  // Calculate the distance between each address and the specified location
+  var origins = addresses.map((address) => ({ lat: address.lat, lng: address.lng }));
+  var destinations = [{ lat: coordinates.lat, lng: coordinates.lng }];
+  var serviceOptions = {
+    origins,
+    destinations,
+    travelMode: 'DRIVING',
+  };
+  var serviceCallback = (response) => {
+    if (response && response.rows) {
+      var distances = response.rows.map((row, index) => ({
+        address: addresses[index],
+        distance: row.elements[0].distance.value,
+      }));
+
+      // Sort the addresses based on their distance from the specified location
+      distances.sort(function (a, b) {
+        return a.distance - b.distance;
+      });
+
+      setSortedAddresses(distances.map((item) => item.address));
+    }
+  };
+  useDistanceMatrixService(serviceOptions, serviceCallback);
+
+  return sortedAddresses;
+};
 
 const GoogleMap = ({ zipCode, zoom }) => {
   const [coordinates, setCoordinates] = useState(null);
   const [mapContainer, setMapContainer] = useState(null);
-  const [apiKey] = useState('AIzaSyBMXY7naDI_g0oU-g_o0_YjpxoXwyL9MT4');
+  const [apiKey] = useState('API_KEY');
   const [isLoading, setIsLoading] = useState(true);
+  const [addresses] = useState([
+    // your list of addresses
+  ]);
+  const sortedAddresses = useSortedAddresses(addresses, coordinates);
 
   useEffect(() => {
     // Fetch the latitude and longitude based on the zip code using a geocoding service
     fetchCoordinates(zipCode);
   }, [zipCode]);
 
-  const fetchCoordinates = useCallback(async (zipCode) => {
-    try {
-      const response = await axios.get(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${apiKey}`
-      );
-      const { lat, lng } = response.data.results[0].geometry.location;
-      setCoordinates({ lat, lng });
-    } catch (error) {
-      console.error('Error fetching coordinates:', error);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [apiKey]);
+  const fetchCoordinates = useCallback(
+    async (zipCode) => {
+      try {
+        const response = await axios.get(
+          `https://maps.googleapis.com/maps/api/geocode/json?address=${zipCode}&key=${apiKey}`
+        );
+        const { lat, lng } = response.data.results[0].geometry.location;
+        setCoordinates({ lat, lng });
+      } catch (error) {
+        console.error('Error fetching coordinates:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [apiKey]
+  );
 
   const mapRef = useCallback((node) => {
     node && setMapContainer(node);
@@ -37,7 +75,12 @@ const GoogleMap = ({ zipCode, zoom }) => {
   };
 
   return (
-    <GoogleMapsProvider googleMapsAPIKey={apiKey} mapContainer={mapContainer} mapOptions={mapOptions}>
+    <GoogleMapsProvider
+      googleMapsAPIKey={apiKey}
+      mapContainer={mapContainer}
+      mapOptions={mapOptions}
+      libraries={['geometry']}
+    >
       {isLoading ? (
         <div className="d-flex justify-content-center align-items-center" style={{ height: '100%' }}>
           <div className="spinner-border" role="status">
@@ -47,6 +90,12 @@ const GoogleMap = ({ zipCode, zoom }) => {
       ) : (
         <React.StrictMode>
           <div ref={mapRef} style={{ height: '100%' }} />
+          {/* Display the sorted addresses */}
+          <ul>
+            {sortedAddresses.map((address) => (
+              <li key={address}>{address}</li>
+            ))}
+          </ul>
         </React.StrictMode>
       )}
     </GoogleMapsProvider>
